695b94a0dbc9f57f553c0e8199bbc219fd2520f6
peterpeterparker <david.dalbusco@outlook.com>
2022-05-06 13:43:09 +0200

    chore: prettier

--- src/buckets_sample_assets/src/index.js
+++ src/buckets_sample_assets/src/index.js
@@ -1,49 +1,50 @@
-import {Actor, HttpAgent} from "@dfinity/agent";
-import {buckets_sample} from "../../declarations/buckets_sample";
-import {idlFactory} from "../../declarations/bucket";
+import { Actor, HttpAgent } from '@dfinity/agent';
+import { buckets_sample } from '../../declarations/buckets_sample';
+import { idlFactory } from '../../declarations/bucket';
 
-export const createBucketActor = async ({canisterId}) => {
-    const agent = new HttpAgent();
+export const createBucketActor = async ({ canisterId }) => {
+	const agent = new HttpAgent();
 
-    if(process.env.NODE_ENV !== "production") {
-        await agent.fetchRootKey();
-    }
+	if (process.env.NODE_ENV !== 'production') {
+		await agent.fetchRootKey();
+	}
 
-    return Actor.createActor(idlFactory, {
-        agent,
-        canisterId
-    });
+	return Actor.createActor(idlFactory, {
+		agent,
+		canisterId
+	});
 };
 
 let bucket;
 
 const initCanister = async () => {
-    try {
-        bucket = await buckets_sample.init();
-        console.log('New bucket:', bucket.toText());
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-const sayHello = async () => {
-    try {
-
-        console.log(bucket);
-
-        const actor = await createBucketActor({canisterId: bucket});
-        console.log(await actor.say());
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-const init = () => {
-  const btnInit = document.querySelector("button#init");
-  btnInit.addEventListener("click", initCanister);
-
-    const btnSay = document.querySelector("button#say");
-    btnSay.addEventListener("click", sayHello);
+	try {
+		bucket = await buckets_sample.init();
+		console.log('New bucket:', bucket.toText());
+	} catch (err) {
+		console.error(err);
+	}
 };
 
-document.addEventListener("DOMContentLoaded", init);
+const sayHello = async () => {
+	try {
+		console.log(bucket);
+
+		const actor = await createBucketActor({
+			canisterId: bucket
+		});
+		console.log(await actor.say());
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+const init = () => {
+	const btnInit = document.querySelector('button#init');
+	btnInit.addEventListener('click', initCanister);
+
+	const btnSay = document.querySelector('button#say');
+	btnSay.addEventListener('click', sayHello);
+};
+
+document.addEventListener('DOMContentLoaded', init);
